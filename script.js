6ffd3d73c964ed8481ba76c9cb041f2d1d300a90
jebinjackson862-cloud <jebinjackson862@gmail.com>
2026-07-27 19:13:10 +0530

    Add WalletWise Version 2 features

--- script.js
+++ script.js
@@ -1,39 +1,138 @@
-let balance = 0;
-let income = 0;
-let expense = 0;
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
 const form = document.getElementById("form");
 const list = document.getElementById("list");
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", addTransaction);
 
-    e.preventDefault();
+function addTransaction(e){
 
-    const type = document.getElementById("type").value;
-    const desc = document.getElementById("desc").value;
-    const amount = Number(document.getElementById("amount").value);
+e.preventDefault();
 
-    if(type === "income"){
-        income += amount;
-        balance += amount;
-    }else{
-        expense += amount;
-        balance -= amount;
-    }
+const transaction={
 
-    document.getElementById("income").innerText = "₹" + income;
-    document.getElementById("expense").innerText = "₹" + expense;
-    document.getElementById("balance").innerText = "₹" + balance;
+id:Date.now(),
 
-    const li = document.createElement("li");
+type:document.getElementById("type").value,
 
-    li.innerHTML = `
-        <span>${desc}</span>
-        <span>${type === "income" ? "+" : "-"}₹${amount}</span>
-    `;
+desc:document.getElementById("desc").value,
 
-    list.appendChild(li);
+amount:Number(document.getElementById("amount").value),
 
-    form.reset();
+category:document.getElementById("category").value,
+
+date:document.getElementById("date").value
+
+};
+
+transactions.push(transaction);
+
+saveData();
+
+displayTransactions();
+
+updateSummary();
+
+form.reset();
+
+}
+
+function displayTransactions(){
+
+list.innerHTML="";
+
+transactions.forEach(transaction=>{
+
+const li=document.createElement("li");
+
+li.innerHTML=`
+
+<div>
+
+<strong>${transaction.desc}</strong><br>
+
+<small>${transaction.category} | ${transaction.date}</small>
+
+</div>
+
+<div>
+
+<span>
+
+${transaction.type==="income" ? "+" : "-"}₹${transaction.amount}
+
+</span>
+
+<button class="delete"
+
+onclick="deleteTransaction(${transaction.id})">
+
+❌
+
+</button>
+
+</div>
+
+`;
+
+list.appendChild(li);
 
 });
+
+}
+
+function deleteTransaction(id){
+
+transactions=transactions.filter(item=>item.id!==id);
+
+saveData();
+
+displayTransactions();
+
+updateSummary();
+
+}
+
+function updateSummary(){
+
+let income=0;
+
+let expense=0;
+
+transactions.forEach(item=>{
+
+if(item.type==="income"){
+
+income+=item.amount;
+
+}else{
+
+expense+=item.amount;
+
+}
+
+});
+
+document.getElementById("income").innerText="₹"+income;
+
+document.getElementById("expense").innerText="₹"+expense;
+
+document.getElementById("balance").innerText="₹"+(income-expense);
+
+}
+
+function saveData(){
+
+localStorage.setItem(
+
+"transactions",
+
+JSON.stringify(transactions)
+
+);
+
+}
+
+displayTransactions();
+
+updateSummary();
